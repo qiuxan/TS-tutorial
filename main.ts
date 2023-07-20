3f18f436a27c81f66ef02306625eb08221147fa8
qiuxan <qiuxan@gmail.com>
2023-07-20 14:25:30 +1000

    8- Interfaces

--- main.ts
+++ main.ts
@@ -12,10 +12,37 @@
 // };
 
 // let backGroundColor = Color.green;
-let message = "abe";
-let endsWithC = message.endsWith("c");
+// let message = "abe";
+// let endsWithC = message.endsWith("c");
 
-let message2;
-message2 = "abc";
-let endsWithC2 = (<string>message).endsWith("c");
-let endsWithC2AlternativeWay = (message as string).endsWith("c");
+// let message2;
+// message2 = "abc";
+// let endsWithC2 = (<string>message).endsWith("c");
+// let endsWithC2AlternativeWay = (message as string).endsWith("c");
+
+// let drawPoint = (x, y, a, b, c, d, e, f, g, h) => {}; //xxxx not good
+
+// let drawPoint = (point) => {};
+
+// drawPoint({ x: 1, y: 2 });
+// //looks good but what if:
+
+// drawPoint({ name: "ian" }); // it will break the code
+
+// solution:
+
+// let drawPoint = (point: { x: number; y: number }) => {};
+
+// solve the issue but what if we have a,b,c,d,e,f,g as parameter?  and also we might have to do it everytime:
+
+// final solution:
+
+interface Point {
+  x: number;
+  y: number;
+}
+let drawPoint = (point: Point) => {
+  console.log(point.x);
+  console.log(point.y);
+};
+
