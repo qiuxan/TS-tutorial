6dc52f3d52889257099c021aeed6830d743b5721
qiuxan <qiuxan@gmail.com>
2023-07-20 13:25:01 +1000

    6- Type Assertions to tell the ts call ide

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
-enum Color {
-  red,
-  green,
-  blue,
-}
+// enum Color {
+//   red,
+//   green,
+//   blue,
+// }
 
-// same with
-let Color2 = {
-  red: 0,
-  green: 1,
-  blue: 2,
-};
+// // same with
+// let Color2 = {
+//   red: 0,
+//   green: 1,
+//   blue: 2,
+// };
 
-let backGroundColor = Color.green;
+// let backGroundColor = Color.green;
+let message = "abe";
+let endsWithC = message.endsWith("c");
+
+let message2;
+message2 = "abc";
+let endsWithC2 = (<string>message).endsWith("c");
+let endsWithC2AlternativeWay = (message as string).endsWith("c");
